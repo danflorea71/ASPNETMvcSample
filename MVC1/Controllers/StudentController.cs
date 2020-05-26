715c0c3d857c2dce228ed1edf5ef064ff5d0bf52
Dan FLOREA <Dan.FLOREA@bdc.ca>
2020-05-25 20:03:51 -0400

    Add StudentDB project and connect the web app to database.

--- MVC1/Controllers/StudentController.cs
+++ MVC1/Controllers/StudentController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MVC1.DataAccessLayer;
 
 namespace MVC1.Controllers
 {
@@ -13,80 +16,155 @@ namespace MVC1.Controllers
         // GET: Student
         public ActionResult Index()
         {
-            //MvcApplication.studentsList.Clear();
-
-            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 1, StudentName = "Dan", Age = 18 });
-            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 2, StudentName = "Calin", Age = 28 });
-
-            return View(MvcApplication.studentsList);
+            //return View(MvcApplication.studentsList);
+            return View(DBContext.SelectAllStudents());
         }
 
         // GET: Student/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Models.Student student = DBContext.SelectStudentById(id);
+
+            if(student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
         }
 
         // GET: Student/Create
         public ActionResult Create()
         {
+
             return View();
         }
 
         // POST: Student/Create
         [HttpPost]
-        public ActionResult Create(MVC1.Models.Student student)
+        public ActionResult Create(Models.Student student)
         {
             try
             {
                 // TODO: Add insert logic here
-                student.StudentId = ++ MvcApplication.globalStudentId;
-                MvcApplication.studentsList.Add(student);
+                //student.StudentId = ++ MvcApplication.globalStudentId;
+                //MvcApplication.studentsList.Add(student);
+                if(ModelState.IsValid)
+                {
+                    if(student == null) { return HttpNotFound(); }
 
-                return RedirectToAction("Index");
+                    DBContext.CreateStudent(student);
+
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again");
+
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                Models.Student student = DBContext.SelectStudentById(id);
+                if (student == null) { return HttpNotFound(); }
+
+                return View(student);
             }
             catch
             {
                 return View();
             }
-        }
-
-        // GET: Student/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
         }
 
         // POST: Student/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int? id, Models.Student student)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (student == null) { return HttpNotFound(); }
+
             try
             {
                 // TODO: Add update logic here
+                if(ModelState.IsValid)
+                {
+                    DBContext.UpdateStudentById(id, student);
+                    return RedirectToAction("Index");
+                }
 
-                return RedirectToAction("Index");
+                
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again");
+
+            }
+
+            return View(student);
+
+
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                Models.Student student = DBContext.SelectStudentById(id);
+                if (student == null) { return HttpNotFound(); }
+
+                return View(student);
             }
             catch
             {
                 return View();
             }
-        }
-
-        // GET: Student/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
+            
         }
 
         // POST: Student/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int? id, Models.Student student)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (student == null) { return HttpNotFound(); }
+
             try
             {
                 // TODO: Add delete logic here
-
+                DBContext.DeleteStudentById(id);
                 return RedirectToAction("Index");
             }
             catch
---
+++ MVC1/DataAccessLayer/DBContext.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.DataAccessLayer
+{
+    public class DBContext
+    {
+        private static SqlConnection GetSqlConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["StudentDBConnection"].ToString();
+
+            return new SqlConnection(connectionString);
+
+        }
+
+        public static IEnumerable<Models.Student> SelectAllStudents()
+        {
+            IEnumerable<Models.Student> studentList = new List<Models.Student>();
+  
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Select * From Student";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        Models.Student student;
+                        while (dataReader.Read())
+                        {
+                            student = new Models.Student();
+                            student.StudentId = (int)dataReader.GetValue(0);
+                            student.StudentName = (string)dataReader.GetValue(1);
+                            student.Age = (int)dataReader.GetValue(2);
+
+                            //(studentList.ToList<Models.Student>()).Add(student);
+                            ((List<Models.Student>)studentList).Add(student);
+                        }
+                    }
+                }
+            }
+
+            return studentList;
+        }
+
+        public static int CreateStudent(Models.Student student)
+        {
+
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Insert into Student(StudentName,Age) values ('{student.StudentName}',{student.Age})";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+
+        }
+
+        public static Models.Student SelectStudentById(int? studentId)
+        {
+            Models.Student studentInfo = new Models.Student();
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Select * From Student Where Id = {studentId}";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            studentInfo.StudentId = (int)dataReader.GetValue(0);
+                            studentInfo.StudentName = (string)dataReader.GetValue(1);
+                            studentInfo.Age = (int)dataReader.GetValue(2);
+                           
+                        }
+                    }
+                }
+            }
+
+            return studentInfo;
+        }
+
+        public static int UpdateStudentById(int? studentId, Models.Student student)
+        {
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Update Student Set StudentName='{student.StudentName}',Age={student.Age} Where Id = {studentId}";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+        }
+
+        public static int DeleteStudentById(int? studentId)
+        {
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"Delete from Student Where Id = {studentId}";
+
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    cnn.Open();
+
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                }
+            }
+
+            return result;
+        }
+    }
+}
